8c1916c00ecad63960db6b450aa72a45c9a3c2f0
Simon Gagnon <sim0ngagn0n@outlook.com>
2016-05-08 20:54:38 -0400

    source

---
+++ Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Trebuchet MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(115, 239);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 39);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(691, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 35);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "0";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(681, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kilos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(173, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Livres";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(433, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 46);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "=";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 36);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Livres en kilos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 83);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = " Kilos en livres";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(212, 36);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 32);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Fahrenheit en celsius";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(212, 83);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 32);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Celsius en fahrenheit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(376, 36);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(152, 32);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Mètres en pieds";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(376, 83);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(152, 32);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Pieds en mètres";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(540, 36);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(152, 32);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Cm en pouce";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(540, 83);
+            this.button8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(152, 32);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Pouce en cm";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(704, 36);
+            this.button9.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(152, 32);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "Litre  en gallons";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(704, 83);
+            this.button10.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(152, 32);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "Gallons en litre";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(379, 343);
+            this.button11.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(147, 37);
+            this.button11.TabIndex = 15;
+            this.button11.Text = "Convertir";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button11;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(904, 448);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Convertiseur";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        int icpt = 1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            icpt = 1;
+            label1.Text = "Livres";
+            label3.Text = "Kilos";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            icpt = 2;
+            label1.Text = "Kilos";
+            label3.Text = "Livres";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            icpt = 3;
+            label1.Text = "Fahrenheit";
+            label3.Text = "Celsius";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            icpt = 4;
+            label1.Text = "Celsius";
+            label3.Text = "Fahrenheit";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            icpt = 5;
+            label1.Text = "Mètres";
+            label3.Text = "Pieds";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            icpt = 6;
+            label1.Text = "Pieds";
+            label3.Text = "Mètres";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            icpt = 7;
+            label1.Text = "Cm";
+            label3.Text = "Pouce";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            icpt = 8;
+            label1.Text = "Pouce";
+            label3.Text = "Cm";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            icpt = 9;
+            label1.Text = "Litre";
+            label3.Text = "Gallons";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            icpt = 10;
+            label1.Text = "Gallons";
+            label3.Text = "Litre";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            double iNb =0;
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "0";
+            }
+            bool res = Double.TryParse(textBox1.Text, out iNb);
+            if (res == false)
+            {
+                MessageBox.Show("Erreur : entrer un chiffre valide.");
+            }
+
+            switch(icpt)
+            {
+                case 1:
+                    label4.Text = Convert.ToString(Math.Round(iNb / 2.2046, 2));
+                    break;
+                case 2:
+                    label4.Text = Convert.ToString(Math.Round(iNb * 2.2046, 2));
+                    break;
+                case 3:
+                    label4.Text = Convert.ToString(Math.Round((iNb - 32) * 5 / 9, 2));
+                    break;
+                case 4:
+                    label4.Text = Convert.ToString(Math.Round(iNb * 9 / 5 + 32, 2));
+                    break;
+                case 5:
+                    label4.Text = Convert.ToString(Math.Round(iNb * 3.2808, 2));
+                    break;
+                case 6:
+                    label4.Text = Convert.ToString(Math.Round(iNb * 0.3048, 2));
+                    break;
+                case 7:
+                    label4.Text = Convert.ToString(Math.Round(iNb * 0.3937, 2));
+                    break;
+                case 8:
+                    label4.Text = Convert.ToString(Math.Round(iNb * 2.54, 2));
+                    break;
+                case 9:
+                    label4.Text = Convert.ToString(Math.Round(iNb * 0.264, 2));
+                    break;
+                case 10:
+                    label4.Text = Convert.ToString(Math.Round(iNb * 3.785, 2));
+                    break;
+            }
+
+        }
+
+    }
+}
